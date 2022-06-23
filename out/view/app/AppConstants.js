f1afc33f92e367f0a05a69b452668277e808d04c
syed <shahid231197@gmail.com>
2022-06-23 18:07:49 +0200

    ignored the out dir for github

--- out/view/app/AppConstants.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.ThemeType = void 0;
-var ThemeType;
-(function (ThemeType) {
-    ThemeType["defaultTheme"] = "surveyViewerDefaultTheme";
-    ThemeType["tekenradarTheme"] = "surveyViewerWithTekenradarTheme";
-})(ThemeType = exports.ThemeType || (exports.ThemeType = {}));
-//# sourceMappingURL=AppConstants.js.map
